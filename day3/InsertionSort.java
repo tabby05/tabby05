e96c2baf87f3f153d7fed1e58a7d03b8717ac87e
tabby05 <48792859+tabby05@users.noreply.github.com>
2020-07-01 20:54:43 +0900

    Day3 Source code - Insertion sort

---
+++ day3/InsertionSort.java
@@ -0,0 +1,21 @@
+package day3;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		int[] arr = {2, 1, 4, 3};		// 배열 선언
+		int temp, j, i;
+		
+		for(i = 1 ; i < arr.length; i ++) {
+			temp = arr[i];		// 삽입할 값을 temp에 저장
+			for(j = i - 1; j >= 0 && arr[j] > temp; j--) { 	// 삽입 값이 정렬된 값들보다 작은 경우
+				arr[j+1] = arr[j];							// 값을 바꿔준다
+			}
+			arr[j+1] = temp;
+		}
+		System.out.print("결과값: ");
+		for(i = 0 ; i < arr.length ; i ++) {
+			System.out.print(arr[i] + " ");		// 정렬된 배열 출력
+		}
+
+	}
+}
